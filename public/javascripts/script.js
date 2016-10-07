0efeb45c3d55af7eb6ef0bdaada6a7b8e1ae1356
Dmitry Akentyev <TogusaRusso@gmail.com>
2016-10-07 13:32:35 +0000

    Now using jQuery

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,11 @@
+/* global $ */
+$(function() {
+  $('input').on('click', function() {
+    $(this).parent().toggleClass('checked');
+  });
+});
+
+/*
 window.onload = function() {
   var checkboxes = document.getElementsByTagName('input');
   for (var i = 0; i < checkboxes.length; ++i) {
@@ -12,3 +20,4 @@ function clickHandler() {
     this.parentNode.className = '';
   }
 }
+*/
