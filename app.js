7ef56e6f7f896bd7a468f25b7f61a7be39f1cfba
Dmitry Akentyev <TogusaRusso@gmail.com>
2016-10-07 12:34:41 +0000

    CSS now generates from SCSS

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var exphbs = require('express-handlebars');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express        = require('express');
+var exphbs         = require('express-handlebars');
+var sassMiddleware = require('node-sass-middleware'); 
+var path           = require('path');
+var favicon        = require('serve-favicon');
+var logger         = require('morgan');
+var cookieParser   = require('cookie-parser');
+var bodyParser     = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-var todos = require('./routes/todos');
+var users  = require('./routes/users');
+var todos  = require('./routes/todos');
 
 var app = express();
 
@@ -17,6 +18,14 @@ var app = express();
 // app.set('view engine', 'jade');
 app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: 'layout'}));
 app.set('view engine', 'hbs');
+app.use (
+  sassMiddleware({
+    src:   __dirname + '/sass',
+    dest:  __dirname + '/public',
+    // prefix: '/stylesheets',
+    debug: true,
+   })
+);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
