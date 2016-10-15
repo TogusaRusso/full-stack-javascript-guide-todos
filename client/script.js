14b3ac44238e97df5b1908317113fd560f0bbc9d
Dmitry Akentyev <TogusaRusso@gmail.com>
2016-10-15 08:02:42 +0000

    Adding new todo thru Ajax

--- client/script.js
+++ client/script.js
@@ -1,11 +1,36 @@
 var $ = require('jquery')
 
 $(function () {
+  $(':button').on('click', addTodo)
   $('input').on('click', function () {
     $(this).parent().toggleClass('checked')
   })
+  $(':text').on('keypress', function (e) {
+    var key = e.keyCode
+    if (key === 13 || key === 169) {
+      addTodo()
+      e.preventDefault()
+      e.stopPropagation()
+      return false
+    }
+  })
 })
 
+var addTodo = function () {
+  var text = $('#add-todo-text').val()
+  $.ajax({
+    url: 'api/todos',
+    type: 'POST',
+    data: {text: text},
+    dataType: 'json',
+    success: function (data) {
+      var todo = data.todo
+      var newLiHtml = `<li><input type='checkbox'><span>${todo.text}</span></li>`
+      $('ul').append(newLiHtml)
+      $('#add-todo-text').val('')
+    }
+  })
+}
 /*
 window.onload = function() {
   var checkboxes = document.getElementsByTagName('input');
